44ca97d7c946d98b9dbfa3c98674fed938de0bd8
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-01-03 12:06:14 +0100

    Added a light library based JQuery, used for ajax and dep for maps api, also some css dist

--- public/wp-content/themes/bellvestis/functions.php
+++ public/wp-content/themes/bellvestis/functions.php
@@ -136,6 +136,8 @@ function bellvestis_scripts()
 
     wp_enqueue_script('bellvestis-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
+    wp_enqueue_script('zepto', 'https://cdnjs.cloudflare.com/ajax/libs/zepto/1.2.0/zepto.js', [], false, true);
+
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
