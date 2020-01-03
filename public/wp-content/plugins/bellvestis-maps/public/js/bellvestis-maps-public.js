f2a0448a48b821fbb5736f23df3e76bf51e242b0
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-01-03 12:21:20 +0100

    Added all the logic that adds the map, markers and so on, also added a height to the map

--- public/wp-content/plugins/bellvestis-maps/public/js/bellvestis-maps-public.js
+++ public/wp-content/plugins/bellvestis-maps/public/js/bellvestis-maps-public.js
@@ -1,32 +1,56 @@
-(function( $ ) {
-	'use strict';
+(function($) {
+  "use strict";
+  $.ajax({
+    type: "GET",
+    url: "http://bellvestis.test/wp-json/wp/v2/store",
+    success: function(markers) {
+      var jsonData = markers;
+      /*  Add a maker for every outlet store */
+      for (var i = 0; i < jsonData.length; i++) {
+        addMarker(jsonData[i].acf);
+      }
+    }
+  });
+})(jQuery);
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+// Initialize and add the map
+function initMap() {
+  // Options
+  var options = {
+    zoom: 10,
+    center: {
+      lat: 55.631432,
+      lng: 13.70557
+    }
+  };
 
-})( jQuery );
+  // The map, centered at sjobo
+  map = new google.maps.Map(document.getElementById("map"), options);
+}
+
+function addMarker(props) {
+  var markerOptions = {
+    position: {
+      lat: parseFloat(props["store_latitud"]),
+      lng: parseFloat(props["store_longitud"])
+    },
+    map: map
+  };
+  var marker = new google.maps.Marker(markerOptions);
+
+  if (props.store_marker_icon) {
+    marker.setIcon(props.store_marker_icon);
+  }
+
+  if (props.store_marker_content_box) {
+    var infoContent = {
+      content: props.store_marker_content_box
+    };
+
+    var infoWindow = new google.maps.InfoWindow(infoContent);
+
+    marker.addListener("click", function() {
+      infoWindow.open(map, marker);
+    });
+  }
+}
